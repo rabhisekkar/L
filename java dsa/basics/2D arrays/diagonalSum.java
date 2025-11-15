cc9ee12772c77edf165f180a247735cc4dc24dda
rabhisekkar <abhisekkar24@gmail.com>
2025-11-15 22:46:43 +0530

    diagonal sum code

---
+++ java dsa/basics/2D arrays/diagonalSum.java
@@ -0,0 +1,75 @@
+// author: @rabhisekkar
+// description: print diagonal sum of a 2d array
+
+// diagonal Sum = primary diagonal sum + secondary diagonal sum
+// only works on matrix where rows = cols
+
+// 2d array: 
+// 1 2 3 4 
+// 5 6 7 8
+// 9 10 11 12
+// 13 14 15 16
+
+//diagonal sum = 1+6+11+16+4+7+10+13 = 68
+
+// 2d array:
+// 1 2 3
+// 4 5 6
+// 7 8 9
+// diagonal sum = 1+5(add 5 once)+9+3+7 = 25
+
+public class diagonalSum {
+    //printing array
+    public static void printArray(int matrix[][]){
+        int n=matrix.length, m=matrix[0].length;
+
+        System.out.println("\n2d array: ");
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    //diagonal sum 1
+    //tc = O(n^2)
+    public static void printDiagonalSum(int matrix [][]){
+        int sum=0;
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                if(i==j || i+j==matrix.length-1){
+                    sum+=matrix[i][j];
+                }
+            }
+        }
+        System.out.println("diagonal sum = " + sum);
+    }
+    //main
+    public static void main(String args[]){
+        int matrix[][] = {{1, 2, 3, 4},
+                          {5, 6, 7, 8},
+                          {9, 10, 11, 12},
+                          {13, 14, 15, 16}};
+        printArray(matrix);
+        printDiagonalSum(matrix);
+        int matrix1[][] = {{1, 2, 3},
+                           {4, 5, 6},
+                           {7, 8, 9}};
+        printArray(matrix1);
+        printDiagonalSum(matrix1);
+    }
+}
+
+// output
+// 2d array:
+// 1 2 3 4 
+// 5 6 7 8
+// 9 10 11 12
+// 13 14 15 16
+// diagonal sum = 68
+
+// 2d array:
+// 1 2 3
+// 4 5 6
+// 7 8 9
+// diagonal sum = 25
