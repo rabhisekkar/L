d4cc5767b0c049b1719a5e8dbef6dfce55d29ad2
rabhisekkar <abhisekkar24@gmail.com>
2026-02-03 16:31:24 +0530

    constructors in abstract class

--- java dsa/basics/OOPs/ConstructorsInAbstractClass.java
+++ java dsa/basics/OOPs/ConstructorsInAbstractClass.java
@@ -1,3 +1,49 @@
+/*  author: @rabhisekkar
+    description: demonstration of constructors and its order in abstract class
+
+    Note: Constructor in an abstract class initializes common state
+    and runs before subclass constructors during object creation.
+
+    Constructors behave the same in abstract and normal classes,
+    but abstract class constructors cannot be called directly
+    because abstract classes cannot be instantiated.
+
+*/
+
 public class ConstructorsInAbstractClass {
-    
+    public static void main(String[] args) {
+        // Animal → Horse → Mustang
+        Mustang m = new Mustang();
+        System.out.println(m.color);    // brown
+    }
 }
+
+// abstract class
+abstract class Animal {
+    String color;
+    // constructor
+    Animal(){
+        color = "brown";
+        System.out.println("animal constructor called");
+    }
+}
+
+class Horse extends Animal{
+    // constructor
+    Horse(){
+        System.out.println("horse constructor called");
+    }
+}
+
+class Mustang extends Horse{
+    // constructor
+    Mustang(){
+        System.out.println("Mustang constructor called");
+    }
+}
+
+// output
+// animal constructor called
+// horse constructor called
+// Mustang constructor called
+// brown
