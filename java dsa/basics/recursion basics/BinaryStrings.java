f4438a498b9c2cfa62614e7510da0f240a64ed8e
rabhisekkar <abhisekkar24@gmail.com>
2026-03-04 21:12:12 +0530

    add recursive solution to generate binary strings without consecutive 1s

---
+++ java dsa/basics/recursion basics/BinaryStrings.java
@@ -0,0 +1,31 @@
+/*  author: @rabhisekkar
+    description: print binary strings of length n without consecutive 1s
+
+    TC = O(2^n)
+    SC = O(n)
+*/
+
+public class BinaryStrings {
+
+    public static void printBinStrings(int n, int lastPlace, String str) {
+
+        // base case
+        if (n == 0) {
+            System.out.println(str);
+            return;
+        }
+
+        // place 0
+        printBinStrings(n - 1, 0, str + "0");
+
+        // place 1 only if last digit was 0
+        if (lastPlace == 0) {
+            printBinStrings(n - 1, 1, str + "1");
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        printBinStrings(n, 0, "");
+    }
+}
